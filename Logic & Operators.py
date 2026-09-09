9525dfae7484484862640b5d7353643f374b472c
Kaustav Mukherjee <kaustavmukhe456@gmail.com>
2026-09-09 08:32:58 +0530

    logic & operators theory codes and practice codes

--- Logic & Operators.py
+++ Logic & Operators.py
@@ -27,7 +27,7 @@
 print(True)
 print(False)
 
-# ~bool(): "TRUE" if the value is non-empty or non zero, "False" if the value is empty or zero.
+    # ~bool(): "TRUE" if the value is non-empty or non zero, "False" if the value is empty or zero.
 
 print(bool(123)) #OUTPUT: TRUE
 print(bool("HI")) #OUTPUT: TRUE
@@ -38,17 +38,112 @@ print(bool("")) #OUTPUT: FALSE
 print(bool(None)) #OUTPUT: FALSE
 
 
-# ~ all(), any(): Helps to evaluate values, more than one.
-    # any(): Returns TRUE if atleast one value is TRUE.
-    # all(): Returns TRUE if all values are TRUE.
+    # ~ all(), any(): Helps to evaluate values, more than one.
+        # any(): Returns TRUE if atleast one value is TRUE.
+        # all(): Returns TRUE if all values are TRUE.
 
 email = input("Enter email: ")
 phone = input("Enter phone_no: ")
 username = input("Enter username: ")
-    #Now the codition is like this, that in a website if any one of the above fields are filled, it allows registration.
+        #Now the codition is like this, that in a website if any one of the above fields are filled, it allows registration.
 print(any([email, phone, username])) # OUTPUT will be TRUE if ANY ONE of the above fields are filled.
 
-    #Now the codition is like this, that in a website ionly if all the above fields are filled, it allows registration.
+        #Now the codition is like this, that in a website ionly if all the above fields are filled, it allows registration.
 print(all([email, phone, username])) # OUTPUT will be TRUE if ALL of the above fields are filled.
 
+    # ~ isinstance(): CHecks if a value belongs to a certain data type.
+print(isinstance(123, str))
+print(isinstance(True, str))
 
+print("Hello".endswith("o"))
+
+    # Comparison operators: It compares two values and return True or False based on the result.
+    # == Equal to
+    # != Not Equal
+    # < Less than
+    # > Greater than
+    # <= Less than or Equal
+    # >= Greater than or Equal
+
+print(10 == 10)
+print(10 != 10)
+print(7 > 3)
+print(7 >= 3)
+print(3 < 7)
+print(3 <= 7)
+print(1 < 4 < 6) # Python executes chain operation here, checking each condition one by one from left to right.
+
+        ## Comparison operators cn also be used to compare strings, 
+print("a"== "a")
+print("a" == "A")
+print("a" < "b")
+
+    # Logical operators: Used to combine multiple boolean expressions.
+    # and, or, not
+
+        #~ and : Python returns TRUE only if all the coditions are TRUE.
+        #~ or : Python returns TRUE only if one of the coditions is TRUE.
+        #~ not : It reverses the truth, turns TRUE into False and vice versa
+
+print(3 > 1 and 5 < 1)
+print(3 > 1 and 5 > 1)
+
+print(3 > 1 or 5 < 1)
+print(3 > 1 or 5 > 1)
+print(3 < 1 and 5 < 1)
+
+print(not 3 > 2)
+print(not True)
+print(not False)
+
+name = ""
+print(not name)
+print(not 0)
+
+        ## Mixing the conditions:
+
+            # ~ ORDER OF OPERATION: Controlling the execution order
+            # "and" operator has higher priority than "or" operator.
+            # Paranthesis () has higher priority over and, or.
+
+# Allow access only if the user is logged in or they are guest but they must not be banned.
+
+is_logged_in = True
+is_guest = False
+is_banned = False
+
+print((is_logged_in or is_guest) and not is_banned)
+
+is_logged_in = True
+is_guest = False
+is_banned = False
+
+print((is_logged_in or is_guest) and not is_banned)
+
+    # Membership operators: Used to combine multiple boolean expressions.
+     #~ in : Checks if a value inside another value, like a string, tuple or other sequence.
+
+print("o" in "Python")
+print("f" not in "Python")
+print(3 in[1,2,3])
+
+    # Identity operators: Checks if two variables refer to the same object in the memory.
+
+x = ['a', 'b', 'c']
+y = ['a', 'b', 'c']
+
+print(x == y) #This is just comparing the values of x and y. OUTPUT: TRUE
+print(x is y) #This will return FALSE, since python isn't comparing the values of the variables, it is comparing the separate ID's of the separate objectscreated for x and y.
+
+
+x = 10
+y = 10
+
+print(x == y) #This is just comparing the values of x and y. OUTPUT: TRUE
+print(x is y) #In this case, as the value is very simple python is not going to create separate ID's for x and y. It will create the same id. OUTPUT: TRUE
+
+x = ['a', 'b', 'c']
+y = x
+
+print(x == y) #This is just comparing the values of x and y. OUTPUT: TRUE
+print(x is y) #This will return TRUE as y is assigned the value of x and it is the same object 
--- Practices.py
+++ Practices.py
@@ -22,3 +22,25 @@ print('name: '+ name +' '+'|'+' role: '+ role + ' ' + '|' + ' age: '+ age)
 import random
 x = random.randint(1,100)
 print(isinstance(x,int))
+
+
+# Check if a users name is not empty and the age is grater than or equal to 18
+# Check if the password is at least 8 characters long and does not contain spaces
+# Check if a user's email is not empty, contains '@', and ends with a '.com'
+# Check if a username is a string, is not None, and is longer than 5 characters
+# Check if the user is either an admin or a moderator, and either they're not banned or theey've verified their email.
+
+username = input("Enter username: ")
+password = input("Enter Password: ")
+age = input("Enter age: ")
+email = input("Enter email: ")
+is_admin = True
+is_moderator = False
+is_banned = False
+has_verified_email = True
+
+print(username is not "" and int(age) >= 18)
+print(len(password) >= 8 and " " not in password)
+print(email is not "" and "@" in email and ".com" in email)
+print(isinstance(username, str) and username is not None and len(username)>5)
+print((is_admin or is_moderator) and (not is_banned or has_verified_email))
